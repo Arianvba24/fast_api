505b33d1094337cff061f8d30ae51e4c89a28a27
Arianvba24 <arianvba24@gmail.com>
2025-04-25 01:26:01 +0200

    added procfile

--- alembic/versions/175bef9c0c4b_create_posts_table.py
+++ alembic/versions/175bef9c0c4b_create_posts_table.py
@@ -20,7 +20,7 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.create_table("posts",sa.Column("id",sa.Integer(),nullable=False,primary_key=True),sa.Column("title",sa.String(),nullable=False))
+    op.create_table("posts",sa.Column("id_value",sa.Integer(),nullable=False,primary_key=True),sa.Column("title",sa.String(),nullable=False))
     pass
 
 
